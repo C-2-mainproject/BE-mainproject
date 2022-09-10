4f42dce1b69bbbf19e06131ef9c57c8403bae770
JangDongHa <94433852+JangDongHa@users.noreply.github.com>
2022-09-11 02:01:53 +0900

    Test: 특정 단어장 등수삭조

---
+++ src/test/java/com/wolves/mainproject/service/MyWordStorageServiceTest.java
@@ -0,0 +1,97 @@
+package com.wolves.mainproject.service;
+
+import com.wolves.mainproject.domain.user.User;
+import com.wolves.mainproject.domain.user.UserRepository;
+import com.wolves.mainproject.domain.word.storage.WordStorage;
+import com.wolves.mainproject.domain.word.storage.WordStorageRepository;
+import com.wolves.mainproject.domain.word.storage.category.WordStorageCategory;
+import com.wolves.mainproject.domain.word.storage.category.WordStorageCategoryRepository;
+import com.wolves.mainproject.dto.request.RequestMyWordStorageDto;
+import com.wolves.mainproject.type.StatusType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class MyWordStorageServiceTest {
+    @Autowired
+    private WordStorageCategoryRepository wordStorageCategoryRepository;
+
+    @Autowired
+    private WordStorageRepository wordStorageRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @BeforeEach
+    void makeWordStorage(){
+        User user = userRepository.findById(1L).orElseThrow();
+        WordStorageCategory category = wordStorageCategoryRepository.findById(1L).orElseThrow();
+        WordStorage wordStorage = WordStorage.builder()
+                .title("test")
+                .wordStorageCategory(category)
+                .description("testDescription")
+                .status(StatusType.PRIVATE)
+                .user(user)
+                .build();
+
+        wordStorageRepository.save(wordStorage);
+    }
+
+    /**
+     * @Description : 특정 단어장 수정
+     * @return : 변경 여부
+     * @Author : Jangdongha
+     **/
+    @Test
+    void updateWordStorageTest(){
+        // Given
+        WordStorage wordStorage = wordStorageRepository.findById(1L).orElseThrow();
+        RequestMyWordStorageDto dto = RequestMyWordStorageDto.builder()
+                .title("updateTest")
+                .category("토익")
+                .description("updateTest")
+                .status(true)
+                .build();
+        WordStorageCategory category = wordStorageCategoryRepository.findByName("토익").orElseThrow();
+        // When
+        wordStorage.update(dto, category);
+        WordStorage wordStoragePS = wordStorageRepository.save(wordStorage);
+        // Then
+        assertEquals("updateTest", wordStoragePS.getTitle());
+    }
+
+    /**
+     * @Description : 특정 단어장 삭제
+     * @return : 삭제 여부
+     * @Author : Jangdongha
+     **/
+    @Test
+    void deleteWordStorageTest(){
+        // Given
+        long deleteId = 1L;
+        WordStorage wordStorage = wordStorageRepository.findById(deleteId).orElseThrow();
+        // When
+        wordStorageRepository.delete(wordStorage);
+        // Then
+        assertNull(wordStorageRepository.findById(deleteId).orElse(null));
+    }
+
+    /**
+     * @Description : 특정 단어장 조회
+     * @return : 조회 여부
+     * @Author : Jangdongha
+     **/
+    @Test
+    void getWordStorageTest(){
+        // Given
+        long requestId = 1L;
+        // When
+        WordStorage wordStoragePS = wordStorageRepository.findById(requestId).orElse(null);
+        // Then
+        assertNotNull(wordStoragePS);
+    }
+}
