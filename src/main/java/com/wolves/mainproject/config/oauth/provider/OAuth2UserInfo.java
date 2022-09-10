36811e8878e55849493f3105c9cba3ebab64c11c
JangDongHa <94433852+JangDongHa@users.noreply.github.com>
2022-09-10 18:09:46 +0900

    Feat: OAuth2(naver, google) 로그인

---
+++ src/main/java/com/wolves/mainproject/config/oauth/provider/OAuth2UserInfo.java
@@ -0,0 +1,8 @@
+package com.wolves.mainproject.config.oauth.provider;
+
+public interface OAuth2UserInfo {
+    String getProviderId();
+    String getProvider();
+    String getEmail();
+    String getUsername();
+}
