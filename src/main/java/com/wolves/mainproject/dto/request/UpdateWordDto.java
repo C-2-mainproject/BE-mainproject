430efef0c46c690d154e8d7cac14b6c19bfef932
JangDongHa <94433852+JangDongHa@users.noreply.github.com>
2022-09-12 21:49:04 +0900

    Feat: DynamoDB Configuration word, meaning, pronunciation 컬럼 삭제 Related To: #14

--- src/main/java/com/wolves/mainproject/dto/request/UpdateWordDto.java
+++ src/main/java/com/wolves/mainproject/dto/request/UpdateWordDto.java
@@ -1,12 +1,7 @@
 package com.wolves.mainproject.dto.request;
 
-import com.wolves.mainproject.domain.meaning.Meaning;
-import com.wolves.mainproject.domain.prononciation.Prononciation;
-import com.wolves.mainproject.domain.word.Word;
-import com.wolves.mainproject.domain.word.storage.WordStorage;
 import lombok.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @AllArgsConstructor
@@ -20,22 +15,5 @@ public class UpdateWordDto {
     private List<List<String>> pronunciations;
     private List<String> descriptions;
 
-    public List<Word> toWords(WordStorage wordStorage){
-        List<Word> result = new ArrayList<>();
-        descriptions.forEach(description -> words.forEach(word -> result.add(Word.builder().word(word).wordStorage(wordStorage).description(description).build())));
-        return result;
-    }
-
-    public List<Meaning> toMeanings(List<Word> words){
-        List<Meaning> result = new ArrayList<>();
-        meanings.forEach(meaning -> meaning.forEach(mean -> words.forEach(word -> result.add(Meaning.builder().word(word).meaning(mean).build()))));
-        return result;
-    }
-
-    public List<Prononciation> toPronunciations(List<Word> words){
-        List<Prononciation> result = new ArrayList<>();
-        pronunciations.forEach(pronunciation -> pronunciation.forEach(pron -> words.forEach(word -> result.add(Prononciation.builder().word(word).prononciation(pron).build()))));
-        return result;
-    }
 
 }
