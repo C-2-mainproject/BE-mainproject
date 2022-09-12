737fd21f7584a4019aa1c1de11c47a916cff93f1
JangDongHa <94433852+JangDongHa@users.noreply.github.com>
2022-09-12 21:47:09 +0900

    Chore: DynamoDB 의존성 추가 단어와 뜻을 각자 하나의 로우로 관리하기에는 너무 많은 참조가 발생 따라서 NoSQL로 단어와 뜻을 저장하고 ID 값을 wordstorage_id로 저장 Related To: #14

--- src/main/java/com/wolves/mainproject/domain/meaning/MeaningRepository.java
+++
@@ -1,6 +0,0 @@
-package com.wolves.mainproject.domain.meaning;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MeaningRepository extends JpaRepository<Meaning, Long> {
-}
--- src/main/java/com/wolves/mainproject/domain/word/WordMapping.java
+++
@@ -1,7 +0,0 @@
-package com.wolves.mainproject.domain.word;
-
-import com.wolves.mainproject.domain.word.storage.WordStorage;
-
-public interface WordMapping {
-    WordStorage getWordStorage();
-}
