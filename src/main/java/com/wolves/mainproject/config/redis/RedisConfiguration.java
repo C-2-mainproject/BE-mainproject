8158848b758a998f7e1e82a3a75ce7b401dfdc73
warmth424 <warmth424@gmail.com>
2022-09-14 22:55:44 +0900

    Prod: EC2 HealthyCheck controller 추가

--- src/main/java/com/wolves/mainproject/config/redis/RedisConfiguration.java
+++ src/main/java/com/wolves/mainproject/config/redis/RedisConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.session.data.redis.config.ConfigureRedisAction;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
@@ -32,5 +33,10 @@ public class RedisConfiguration {
         template.setConnectionFactory(lettuceConnectionFactory());
         template.setDefaultSerializer(new StringRedisSerializer());
         return template;
+    }
+
+    @Bean
+    public static ConfigureRedisAction configureRedisAction() {
+        return ConfigureRedisAction.NO_OP;
     }
 }
