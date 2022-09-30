9113f1d3848ff1d2a3812b43c2bbe5b41d49f803
NewL <taemin1104@naver.com>
2022-10-01 01:51:56 +0900

    Fix: 회원정보 조회 요청 관련 일부수정

--- src/main/java/com/wolves/mainproject/dto/UserResponseDto.java
+++ src/main/java/com/wolves/mainproject/dto/UserResponseDto.java
@@ -11,6 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UserResponseDto {
 
+    private String username;
     private String nickname;
     private String profileImage;
 
